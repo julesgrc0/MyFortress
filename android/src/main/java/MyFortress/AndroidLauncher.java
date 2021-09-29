1bf1379a1b2147775d8211122551b9f4831009cd
JulesG10 <jules10avrg@gmail.com>
2021-09-29 14:50:20 +0200

    generate a libGDX project with gdx-liftoff

---
+++ android/src/main/java/MyFortress/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package MyFortress;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import MyFortress.MainGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), configuration);
+	}
+}
